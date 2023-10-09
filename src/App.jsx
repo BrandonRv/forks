1dfe11323d20986d358dbe6549942fa8e5ff6690
harold2001 <hcarazasnires@gmail.com>
2023-10-09 09:33:03 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Fork</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Clase del día 16 - Forks
+        </p>
+      </div>
+      <p className="read-the-docs">
+        !Éxitos, crack!
+      </p>
+    </>
+  )
+}
+
+export default App
